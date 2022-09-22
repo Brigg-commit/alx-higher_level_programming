52d44573c4c66f60312b604b968dc0ab56948968
“Brigg-commit” <ibridget95@gmail.com>
2022-09-22 14:23:21 +0100

    This program assigned random signed number to a variable

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+# if number is greater than 0: is positive
+# if number is 0: is zero
+# if number is less than zero: is negative
+#!/bin/usr/python3
+import random
+number = random randint(-1000, 1000)
+if number > 0:
+    print(f"{number:d} is positive")
+elif number == 0:
+    print(f"{number:d} is zero")
+else:
+    print(f"{number:d} is negative)
