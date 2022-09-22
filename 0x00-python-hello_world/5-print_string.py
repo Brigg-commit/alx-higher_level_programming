86591be6f84ec5e9b272b3e059e50b86689f1d7c
“Brigg-commit” <ibridget95@gmail.com>
2022-09-22 08:24:35 +0100

    commit update for task 4,5,6 7 & 8

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,3 +1,4 @@
-#!/usr/bin/python3 
- str = "Holberton School" 
- print(str+str+str+'\n'+str[:9])
+#!/usr/bin/python3
+str = "Holberton School"
+print(str[0:10])
+print(str[10:] + str[0:10] str * 3)
