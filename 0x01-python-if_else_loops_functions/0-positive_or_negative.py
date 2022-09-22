37da71f6ea8f15ec4d71b98180f74498a2b9628d
“Brigg-commit” <ibridget95@gmail.com>
2022-09-22 16:20:44 +0100

    commit correction

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,12 +1,9 @@
-# if number is greater than 0: is positive
-# if number is 0: is zero
-# if number is less than zero: is negative
 #!/bin/usr/python3
 import random
-number = random randint(-1000, 1000)
+number = random.randint(-10, 10)
 if number > 0:
     print(f"{number:d} is positive")
 elif number == 0:
     print(f"{number:d} is zero")
 else:
-    print(f"{number:d} is negative)
+    print(f"{number:d} is negative")
