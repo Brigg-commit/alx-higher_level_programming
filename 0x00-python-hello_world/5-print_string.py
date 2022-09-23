223f1f77036cc454da54a384549588e54ad24763
“Brigg-commit” <ibridget95@gmail.com>
2022-09-23 16:37:12 +0100

    prints three times a string

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
 str1 = str[10:] + str[0:10]
-print(str[0:10], str1 * 2, str[10:] + '\n' + str[0:10])
+print(str[0:10], str1 * 2, str[10:] + '\n', str[0:10])
