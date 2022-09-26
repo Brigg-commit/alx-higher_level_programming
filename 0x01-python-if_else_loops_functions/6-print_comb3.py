1e855564cbf8339d51685486cd028da978db24de
“Brigg-commit” <ibridget95@gmail.com>
2022-09-26 09:18:50 +0100

    commit task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=', ')
-        print("{:d}{:d}".format(i + 1, j))
+for number in range(100):
+    if int(number / 10) != number % 10 and int(number / 10) < number % 10:
+        print("{}{}".format(int(number / 10), number % 10), end="")
+        if (number != 89):
+            print(", ",  end="")
