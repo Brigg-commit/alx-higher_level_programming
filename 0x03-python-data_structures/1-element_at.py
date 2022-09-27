ed89aba14e50efb3a2148885d10243fb05d33c93
“Brigg-commit” <ibridget95@gmail.com>
2022-09-27 11:34:01 +0100

    function that retrieves an element from C

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+# funtion that retrieves an element from a list in C
 def replace_in_list(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return
+    if idx < 0:
+        return (None)
+    length = len(my_list)
+    if idx > Length - 1:
+        return (None)
     else:
         return (my_list[idx])
