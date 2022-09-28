66d1f1da541f4a54a006ecf64de97f58d0149864
“Brigg-commit” <ibridget95@gmail.com>
2022-09-28 18:35:00 +0100

    commit task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_l = my_list[:]
+    new_l = my_list.copy()
     for x in range(0, len(my_list)):
+        list_result = divisible_by_2(list)
         if my_list[x] % 2 == 0:
             new_l[x] = 1
         else:
