1b62bf0ab0d9adce1ac3ce000c7ac99d398d6627
“Brigg-commit” <ibridget95@gmail.com>
2022-10-01 17:54:54 +0100

    function that replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matric = matrix[:]
+    new_matrix[i] = matrix.copy()
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-        return (new_matrix)
+        return (new_matrix[i])
