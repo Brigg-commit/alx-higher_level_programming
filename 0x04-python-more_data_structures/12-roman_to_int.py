eb33d60a4827ec1028aa7433904fa30235c4d12d
“Brigg-commit” <ibridget95@gmail.com>
2022-10-02 23:50:35 +0100

    commit tasks on more data structure project

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    for i in range(1, 3999):
+        if roman_number != roman_string:
+            return (None)
+        else:
+            return (roman_number)
