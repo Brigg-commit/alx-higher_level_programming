104e476054b9ec56d348429a4363b59b171231b8
“Brigg-commit” <ibridget95@gmail.com>
2022-10-05 16:06:47 +0100

    commit 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str = str[36:66] + str[104:107] + str[:6]
+str = str[36:66] + str[102:105] + str[:6]
 print(str)
