f10ca3ed8e308601016d6ca830b3837d30d58b9a
“Brigg-commit” <ibridget95@gmail.com>
2022-10-06 03:42:33 +0100

    commit task 6, 8,9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number >= 0:
-        digit = number % 10
-    else:
-        digit = number % -10
-        digit *= -1
-        print("{:d}".format(digit), end='')
-        return (digit)
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
