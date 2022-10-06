f33aa5f7b1c03a9ffde311d16df666d1b54b1a8e
“Brigg-commit” <ibridget95@gmail.com>
2022-10-06 16:22:28 +0100

    commit import functions

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    result = a + b
-    result_sub = a - b
-    result_mul = a * b
-    result_div = a / b
-    print("{:d} + {:d} = {:d}".format(a, b, result))
-    print("{:d} - {:d} = {:d}".format(a, b, result_sub))
-    print("{:d} * {:d} = {:d}".format(a, b, result_mul))
-    print("{:d} / {:d} = {:d}".format(a, b, result_div))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
