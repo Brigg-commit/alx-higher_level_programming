dbac5fdfe5425dd1432244dc558ec323eb4d1f01
“Brigg-commit” <ibridget95@gmail.com>
2022-10-06 19:35:20 +0100

    commit all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("{} arguments.".format(count))
-    elif count == 1:
-        print("{} argument:".format(count))
-    else:
-        print("{}: arguments".format(count))
-        if count >= 1:
-            for arg in sys.argv:
-                if count != 0:
-                    print("{}: {}".format(count, arg))
-                    count += 1
+    from sys import argv
+    length = len(argv)
+    print("{:d} {:s}{:s}".format(length - 1, "argument" if length <= 2 else "arguments", "." if length == 1 else ":"))
+    for i, s in enumerate(argv):
+        if i > 0:
+            print("{:d}: {:s}".format(i, s))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for t in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
-            print(name)
+        if s[:2] != "__":
+            print("{:s}".format(s))
