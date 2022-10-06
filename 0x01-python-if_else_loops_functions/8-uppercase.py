9183cd355c8a75707cae1badc53abd3a75f5b2b1
“Brigg-commit” <ibridget95@gmail.com>
2022-10-06 07:53:59 +0100

    commit 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            i = chr(ord(str[i]) - 32)
-            print("{}".format(i), end="")
-                print()
+        if ord('a') >= 97 and ord('z') <= 122:
+            i = chr(ord(i) - (ord('a') - ord('A'))
+                    print("{:s}".format(i), end="")
+                    print("")
