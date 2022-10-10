3b036b470d0e9fa88f93937263222c8578987cfc
“Brigg-commit” <ibridget95@gmail.com>
2022-10-10 14:51:07 +0100

    commit all

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # 'function that returns a new dictionary with all values multiplied by 2'
- def multiply_by_2(my_dict):
-     return {key: value * 2 for key, value in my_dict.items()}
+def multiply_by_2(my_dict):
+    return {key: value * 2 for key, value in my dict.items()}
