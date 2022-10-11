b37994254db9bf8e6110292051f40d20bba5baaa
“Brigg-commit” <ibridget95@gmail.com>
2022-10-11 17:08:03 +0100

    commit task 3 to 6

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        ret = a / b
+    except ZeroDivisionError:
+        ret = None
+    finally:
+        print("Inside result: {}".format(ret))
+        return ret
---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+"""Raise a TypeError exception."""
+raise TypeError
