272325cd61007ae37edd62d43146f344ae54919f
“Brigg-commit” <ibridget95@gmail.com>
2022-10-11 10:29:46 +0100

    function that prints an interger

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     """Print x elememts of a list.
     Args:
@@ -6,12 +7,12 @@ def safe_print_list(my_list=[], x=0):
         Returns:
         The number of elements printed.
         """
-        count = 0
-        for z in range(x):
-            try:
-                print("{}".format(my_list[z], end="")
-                        count += 1
-                        except: IndexError:
-                         break
-                         print("")
-                         return (count)
+    count = 0
+    for z in range(x):
+        try:
+            print("{}".format(my_list[z], end="")
+                  count += 1
+                  except IndexError:
+                  break
+                  print("")
+                  return (count)
