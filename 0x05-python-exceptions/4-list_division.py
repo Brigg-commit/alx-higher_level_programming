a8afbc7e552521462c0f638591043f0b118892d2
“Brigg-commit” <ibridget95@gmail.com>
2022-10-11 20:12:57 +0100

    commit 7

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    va = 0
-    length = []
-    while va < list_length:
+    new_list = []
+    for i in range(0, list_length):
         try:
-            res = my_list_1[va] / my_list_2[va]
+            res = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            res = 0
+            div = 0
             print("division by 0")
             except TypeError:
-            res = 0
+            div = 0
             print("wrong type")
         except IndexError:
-            res = 0
+            div = 0
             print("out of range")
+            div = 0
         finally:
-            va += 1
-            length.append(res)
-    return length
+            new_list.append(div)
+    return new_list
