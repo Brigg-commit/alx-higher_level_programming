304455b1601a687c21288c6810d4892eac6ccd27
“Brigg-commit” <ibridget95@gmail.com>
2022-10-19 06:19:32 +0100

    commit 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    nargs = len(sys.argv)-1
-    if nargs != 3:
+    nargs = len(sys.argv)
+    if nargs != 4
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        quit(1)
         op = sys.argv[2]
         if op != '+' and op != '-' and op != '*' and op != '/':
             print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-        from calculator_1 import add, sub, mul, div
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if op == '+':
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif op == '-':
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif op == '*':
-            print("{} - {} = {}".format(a, b, mul(a, b)))
-        else:
-            print("{} / {} = {}".format(a, b, div(a, b)))
+            quit(1)
+            from calculator_1 import add, sub, mul, div
+            a = int(argv[1])
+            b = int(argv[3]
+                    [funcs = [add, sub, mul, div]
+                    op = sys.argv[2]
+                    print("{} {} {} = {}".format(a, op, b, funcs[i](a, b)))
