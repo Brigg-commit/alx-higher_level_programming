9dc879188ad0efdfd3f9961840ccd1f6d86e0206
“Brigg-commit” <ibridget95@gmail.com>
2022-12-10 14:30:43 +0100

    commit task 17 to 20

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,7 @@
--- Lists all genres from hbtn_0d_tvshows
--- Record should display: <TV Show genre> - <Number of shows linked to this genre>
--- Results must be sorted in descending order by order of show linked.
--- You can use only one SELECT statement
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
-FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_shows DESC;
-
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+-- lists all rows of a database meeting a condition
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
+FROM tv_genres RIGHT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,13 +1,10 @@
--- Creates the database hbtn_0d_usa
--- If database  already exists, script should not fail
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa
-
--- Creates table cities in the database hbtn_0d_usa
--- id INT unique, auto generated, can’t be null and is a primary key
--- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
--- name VARCHAR(256) can’t be null
--- if table exists, script should not fail.
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
+USE hbtn_0d_usa;
+-- creates a table
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
 PRIMARY KEY(id),
