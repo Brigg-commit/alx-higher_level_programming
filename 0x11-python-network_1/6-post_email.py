50d294e3dd38e632c1565c8033d9e7ae1a7c9987
“Brigg-commit” <ibridget95@gmail.com>
2023-01-31 10:34:18 +0100

    commit updates

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """Sends a POST request to a given URL with a given email.
-Usage: ./6-post_email.py <URL> <email>
-  - Displays the body of the response.
-"""
-#!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./6-post_email.py <URL> <email>
+Usage: ./6-post_email..py <URL> <email>
   - Displays the body of the response.
 """
 import sys
@@ -14,7 +9,7 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email": sys.argv[1]}
+    value = {"email": sys.argv[2]}
 
     r = requests.post(url, data=value)
     print(r.text)
